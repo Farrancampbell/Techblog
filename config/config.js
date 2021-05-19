9f46b71f1e0a92febe7395f87acf232926cc16d3
Farrancampbell <farranerinandcrew@gmail.com>
2021-05-19 10:11:46 -0400

    made changes

--- config/config.js
+++ config/config.js
@@ -1,7 +1,28 @@
-module.exports = {
-    production: {
-      use_env_variable: 'JAWSDB_URL',
-      dialect: 'mysql',
-    },
-  };
+// module.exports = {
+//     production: {
+//       use_env_variable: 'JAWSDB_URL',
+//       dialect: 'mysql',
+//     },
+//   };
   
+  const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
